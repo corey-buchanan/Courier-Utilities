11602ecfa9abef7515b24e315c62acc1a90019bc
Corey Buchanan <coreybuchanan.engineer@gmail.com>
2020-06-21 18:38:52 -0600

    Modify readme to better describe project.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      stage: 0,
+      currentText: "",
+      moneyPerWeek: 0,
+      hoursPerWeek: 0,
+      deliveriesPerHour: 0,
+      continuePlanningPressed: false
+    }
+    this.handleAdvance = this.handleAdvance.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+
+  }
+
+  handleChange(e) {
+    this.setState({currentText: e.target.value})
+  }
+
+  handleAdvance(e) {
+    e.preventDefault() // Keeps the page from refreshing
+
+    switch (this.state.stage) {
+      case 0:
+      this.setState({moneyPerWeek: Number(this.state.currentText)})
+      break
+      case 1:
+      this.setState({hoursPerWeek: Number(this.state.currentText)})
+      break;
+      case 2:
+      this.setState({deliveriesPerHour: Number(this.state.currentText)})
+      break;
+    }
+
+    this.setState({currentText: "", stage: this.state.stage + 1})
+  }
+
+  render() {
+    if (this.state.stage < 3) {
+      return(<WelcomeQuestions
+        stage={this.state.stage}
+        text={this.state.currentText}
+        onChange={this.handleChange}
+        onAdvance={this.handleAdvance}/>)
+    } else {
+      let minimumAmount = this.state.moneyPerWeek / this.state.hoursPerWeek / this.state.deliveriesPerHour
+      return(<MinimumOrder
+        minimumAmount={minimumAmount}/>)
+    }
+
+  }
+
+}
+
+
+// Handles display of question prompts
+class WelcomeQuestions extends React.Component {
+
+  render() {
+
+    let labelText
+
+    switch(this.props.stage) {
+      case 0:
+      labelText = "How much money do you want to make this week?"
+      break
+      case 1:
+      labelText = "How many hours do you want to work this week?"
+      break
+      case 2:
+      labelText = "How many deliveries on average can you complete per hour?"
+      break
+      default:
+      labelText = "An error occured" + this.props.stage
+    }
+
+    return(
+      <form onSubmit={this.props.onAdvance}>
+        <label>{labelText}</label><br />
+        <input onChange={this.props.onChange} value={this.props.text}/>
+      </form>
+    )
+
+  }
+
+}
+
+class MinimumOrder extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>You need to make</h1>
+        <h2>{this.props.minimumAmount}</h2>
+        <h1>per order to reach your goal.</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
